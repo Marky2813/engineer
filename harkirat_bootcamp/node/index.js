499d7bc176e2322e954e99bd7395f3d52d8f1f6e
Sarthak <messagetosarthak@gmail.com>
2026-03-10 20:24:04 +0530

    Full Stack Calculator Complete

--- harkirat_bootcamp/node/index.js
+++ harkirat_bootcamp/node/index.js
@@ -5,6 +5,9 @@ let path = require('path')
 
 const port = 3000;
 
+app.use(express.json());
+//function which runs between the request arriving and the handler running.  
+
 app.get("/", (req, res) => {
   // console.log(req.query.a, req.query.b)
   // const sum = Number(req.query.a) + Number(req.query.b);
@@ -12,11 +15,26 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-app.get("/cal", (req, res) => {
+app.post("/cal", (req, res) => {
   // console.log(req.query.a, req.query.b)
   // const sum = Number(req.query.a) + Number(req.query.b);
   // res.send(`<h2>${sum}</h2>`);
-  res.json("response has been recieved!")
+  console.log(req.body)
+  let a = req.body.a; 
+  let b = req.body.b;
+  let result;  
+  if((req.headers["operation"]) === "sum") {
+    result = a+b;
+  } else if((req.headers["operation"]) === "sub") {
+    result = a - b; 
+  } else if((req.headers["operation"]) === "mul") {
+    result = a*b; 
+  } else {
+    result = a/b;
+  }
+  res.json({
+    "result": result.toFixed(2), 
+  })
 })
 
 app.listen(port, () => {
