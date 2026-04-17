22d91e42a6ba9956ad2a72f30d9989e80549c95f
Sarthak <messagetosarthak@gmail.com>
2026-04-17 21:07:45 +0530

    Connection established with postgres db using neon

---
+++ harkirat_bootcamp/sql/index.js
@@ -0,0 +1,38 @@
+
+const express = require('express'); 
+const app = express(); 
+const port = 3000; 
+app.use(express.json());
+require('dotenv').config(); 
+const { Pool } = require('pg'); 
+require('dotenv').config(); 
+
+
+//SQL is sturctured query language used to communicate with a relational databse. 
+//PortgresSQL is a relational database management system
+//neon db is a serverless cloud platform for postgressql
+//pg is a library used to connect and run queries on a postgressql 
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+})
+
+app.get('/', async (req, res) => {
+  let client; 
+  try {
+    client = await pool.connect(); 
+    const { rows } = await client.query('SELECT version()');
+    const version = rows[0]?.version || 'No version found';
+    res.json({ version })  
+  } catch (err) {
+    console.error('database query failed', err); 
+    res.status(500).json({ err})
+  } finally {
+    client?.release(); 
+  }
+})
+
+
+app.listen(port, () => {
+  console.log("listening to port 3000!")
+})
